6e7fb6eea48cdf93032fd9a15473d6a30c922c37
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2024-01-15 12:29:24 +0530

    Added P2P Store API for Node

---
+++ node/helpers/p2p_store.py
@@ -0,0 +1,107 @@
+'''
+Key Value Store Module
+'''
+import json
+import traceback
+from typing import Any
+from time import sleep
+from env import env
+from helpers import http
+from helpers.logging import logger
+
+
+DELAY = env['DELAY']
+KVS_URL = env['P2PSTORE_URL']
+
+#########################
+# Wrapper KVStore API
+#########################
+
+
+def getv(key: str) -> Any:
+    '''
+    Get Value from Key
+    '''
+    value = kv_get_legacy(key)
+
+    return value
+
+
+def setv(value: Any) -> str:
+    '''
+    Set Value and get Key
+    '''
+    key = kv_set_legacy(value)
+
+    return key
+
+
+def delete(key: str) -> Any:
+    '''
+    Delete Value with Key
+    '''
+    value = kv_delete_legacy(key)
+
+    return value
+
+
+###############################
+# HTTP-based Legacy KVStore API
+###############################
+
+def kv_get_legacy(key: str) -> Any:
+    '''
+    Get Value from Key
+    '''
+    while True:
+        try:
+            reply = http.get(f'{KVS_URL}/get',
+                             {'key': key})
+            break
+        except Exception:
+            logger.error(
+                f'KVStore Database Connection Error! Retrying in 30s.\n{traceback.format_exc()}')
+            sleep(DELAY*6)
+
+    if reply['status'] == 404:
+        return None
+
+    return json.loads(reply['value'])
+
+
+def kv_set_legacy(value: Any) -> str:
+    '''
+    Set Value with Key
+    '''
+    while True:
+        try:
+            reply = http.post(f'{KVS_URL}/set', {'value': json.dumps(value)})
+            break
+        except Exception:
+            logger.error(
+                f'KVStore Database Connection Error! Retrying in 30s.\n{traceback.format_exc()}')
+            sleep(DELAY*6)
+
+    if reply['status'] == 500:
+        return None
+
+    return reply['key']
+
+
+def kv_delete_legacy(key: str) -> Any:
+    '''
+    Delete Value with Key
+    '''
+    while True:
+        try:
+            reply = http.get(f'{KVS_URL}/delete', {'key': key})
+            break
+        except Exception:
+            logger.error(
+                f'KVStore Database Connection Error! Retrying in 30s.\n{traceback.format_exc()}')
+            sleep(DELAY*6)
+
+    if reply['res'] == 404:
+        return None
+
+    return json.loads(reply['value'])
