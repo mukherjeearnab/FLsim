7bc92fdfe9373d53b0e5daf7bada21ce017adb25
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2024-01-18 16:31:53 +0530

    Added Console History for Controller

--- controller/main.py
+++ controller/main.py
@@ -1,16 +1,19 @@
 '''
 This is the Commandline interface for managing the server
 '''
+import os
 import sys
+import readline
 import logging
-from time import sleep
-from helpers.logging import logger
-# from helpers import torch as _
 from commands import job
 
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 
+readline.set_history_length(1000)
+comm_history = './.comm_history'
+if os.path.exists(comm_history):
+    readline.read_history_file(comm_history)
 
 WELCOME_PROMPT = '''Welcome to the DistLearn Controller CLI.
 To get started, enter 'help' in the command prompt below.
@@ -18,28 +21,28 @@ To get started, enter 'help' in the command prompt below.
 
 SINGLE_COMMANDS = ['exit']
 
+
 # start the server
 if __name__ == '__main__':
     print(WELCOME_PROMPT)
     while True:
-        print('> ', end='')
-        command = input()
-        args = command.split(' ')
-        if len(args) <= 1 and args[0] not in SINGLE_COMMANDS:
-            continue
+        try:
+            command = input('> ')
+            args = command.split(' ')
+            if len(args) <= 1 and args[0] not in SINGLE_COMMANDS:
+                continue
 
-        # if args[0] == 'server':
-        #     server.handle_command(args[1:])
+            elif args[0] == 'job':
+                job.handle_command(args[1:])
 
-        # elif args[0] == 'client':
-        #     client.handle_command(args[1:])
+            elif args[0] == 'exit':
+                readline.write_history_file(comm_history)
+                print('Exiting...')
+                sys.exit()
+            else:
+                print('Unknown command: ', command)
 
-        elif args[0] == 'job':
-            job.handle_command(args[1:])
-
-        elif args[0] == 'exit':
+        except KeyboardInterrupt:
+            readline.write_history_file(comm_history)
             print('Exiting...')
             sys.exit()
-
-        else:
-            print('Unknown command: ', command)
