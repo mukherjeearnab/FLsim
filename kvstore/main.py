b5a6197479c106777b536ad69a3fb58a17eb75a4
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2024-02-18 16:57:43 +0530

    Added Netowrk Usage Stat | Implemented KVStore size

--- kvstore/main.py
+++ kvstore/main.py
@@ -1,7 +1,7 @@
 '''
 Key Value Store Management Router
 '''
-
+import sys
 from flask import Flask, jsonify, request
 from key_val_store import KeyValueStore
 
@@ -60,6 +60,40 @@ def set_val():
         return jsonify({'status': True, 'key': key}), 200
     except:
         return jsonify({'status': False, 'key': None}), 500
+
+
+@app.route('/size')
+def get_size():
+    '''
+    get the size of the kvstore object
+    '''
+
+    def get_size(obj, seen=None):
+        """Recursively finds size of objects"""
+        size = sys.getsizeof(obj)
+        if seen is None:
+            seen = set()
+        obj_id = id(obj)
+        if obj_id in seen:
+            return 0
+        # Important mark as seen *before* entering recursion to gracefully handle
+        # self-referential objects
+        seen.add(obj_id)
+        if isinstance(obj, dict):
+            size += sum([get_size(v, seen) for v in obj.values()])
+            size += sum([get_size(k, seen) for k in obj.keys()])
+        elif hasattr(obj, '__dict__'):
+            size += get_size(obj.__dict__, seen)
+        elif hasattr(obj, '__iter__') and not isinstance(obj, (str, bytes, bytearray)):
+            size += sum([get_size(i, seen) for i in obj])
+        return size
+
+    try:
+        kv_size = get_size(keyValueStore.table)
+        return jsonify({'status': True, 'size': kv_size}), 200
+    except:
+        return jsonify({'status': False, 'size': -1}), 500
+
 
 
 if __name__ == '__main__':
